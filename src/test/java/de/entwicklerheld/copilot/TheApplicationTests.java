afe8e2464fef46d8ca2b9addfb8322d953102370
fherling <frank.herling@prodyna.com>
2024-12-07 11:16:15 +0100

    Adding testcontainers for UNIT testing

--- src/test/java/de/entwicklerheld/copilot/TheApplicationTests.java
+++ src/test/java/de/entwicklerheld/copilot/TheApplicationTests.java
@@ -2,12 +2,36 @@ package de.entwicklerheld.copilot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class TheApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("test")
+            .withUsername("user")
+            .withPassword("password");
+
+    @Container
+    static GenericContainer<?> artemis = new GenericContainer<>("vromero/activemq-artemis:latest")
+            .withExposedPorts(61616);
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.artemis.broker-url", () -> "tcp://" + artemis.getHost() + ":" + artemis.getMappedPort(61616));
+    }
 
     @Test
     void contextLoads() {
     }
-
 }
