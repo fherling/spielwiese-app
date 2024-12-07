9986d83930c7f961bd2ed6862ba2d8ebfb25f862
fherling <frank.herling@prodyna.com>
2024-12-07 11:06:15 +0100

    Initial commit

---
+++ src/test/java/de/entwicklerheld/copilot/TheApplicationTests.java
@@ -0,0 +1,13 @@
+package de.entwicklerheld.copilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
